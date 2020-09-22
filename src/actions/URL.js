7d3bef58a9c1590cd94d16ef101ef8899f8b1016
gaeremtro <gabripenalva42@gmail.com>
2020-09-22 18:00:30 +0200

    error http

--- src/actions/URL.js
+++ src/actions/URL.js
@@ -2,7 +2,7 @@ const IP = 'https://heired-server.herokuapp.com' // 192.168.0.59
 const URL = {
     GET_ENTRIES: `${IP}/api/entries`,
     POST_LOGIN: `${IP}/api/login`,
-    POST_REGISTER: `${IP}api/register`,
+    POST_REGISTER: `${IP}/api/register`,
     API_USER: `${IP}/api/user`,
     INFO_COMMENTS: `${IP}/info/comments`,
     ADD_ENTRY: `${IP}/api/addEntry`,
