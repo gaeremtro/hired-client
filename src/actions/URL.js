47bb50a4ba96e4e8863a3cd0e185bab2f1b61a64
gaeremtro <gabripenalva42@gmail.com>
2020-09-22 17:57:22 +0200

    error http

--- src/actions/URL.js
+++ src/actions/URL.js
@@ -1,13 +1,13 @@
 const IP = 'https://heired-server.herokuapp.com' // 192.168.0.59
 const URL = {
-    GET_ENTRIES: `http://${IP}/api/entries`,
-    POST_LOGIN: `http://${IP}/api/login`,
-    POST_REGISTER: `http://${IP}api/register`,
-    API_USER: `http://${IP}/api/user`,
-    INFO_COMMENTS: `http://${IP}/info/comments`,
-    ADD_ENTRY: `http://${IP}/api/addEntry`,
-    EDIT: `http://${IP}/api/edit`,
-    ADD_POP: `http://${IP}/api/addPop`,
+    GET_ENTRIES: `${IP}/api/entries`,
+    POST_LOGIN: `${IP}/api/login`,
+    POST_REGISTER: `${IP}api/register`,
+    API_USER: `${IP}/api/user`,
+    INFO_COMMENTS: `${IP}/info/comments`,
+    ADD_ENTRY: `${IP}/api/addEntry`,
+    EDIT: `${IP}/api/edit`,
+    ADD_POP: `${IP}/api/addPop`,
     IP: `${IP}`
 }
 export default URL
