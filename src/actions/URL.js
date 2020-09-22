ea11143d9f52e2391ea1098c95a11191441abb86
gaeremtro <gabripenalva42@gmail.com>
2020-09-22 17:51:20 +0200

    nodemodules

--- src/actions/URL.js
+++ src/actions/URL.js
@@ -1,13 +1,13 @@
-const IP = 'localhost' // 192.168.0.59
+const IP = 'https://heired-server.herokuapp.com/' // 192.168.0.59
 const URL = {
-    GET_ENTRIES: `http://${IP}:4200/api/entries`,
-    POST_LOGIN: `http://${IP}:4200/api/login`,
-    POST_REGISTER: `http://${IP}:4200/api/register`,
-    API_USER: `http://${IP}:4200/api/user`,
-    INFO_COMMENTS: `http://${IP}:4200/info/comments`,
-    ADD_ENTRY: `http://${IP}:4200/api/addEntry`,
-    EDIT: `http://${IP}:4200/api/edit`,
-    ADD_POP: `http://${IP}:4200/api/addPop`,
+    GET_ENTRIES: `http://${IP}api/entries`,
+    POST_LOGIN: `http://${IP}api/login`,
+    POST_REGISTER: `http://${IP}api/register`,
+    API_USER: `http://${IP}api/user`,
+    INFO_COMMENTS: `http://${IP}info/comments`,
+    ADD_ENTRY: `http://${IP}api/addEntry`,
+    EDIT: `http://${IP}api/edit`,
+    ADD_POP: `http://${IP}api/addPop`,
     IP: `${IP}`
 }
 export default URL
